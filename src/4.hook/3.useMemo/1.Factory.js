d4aa0d2fbc09839ac87f37272da47e1aacd51158
yunghyunchoi <asl321@naver.com>
2023-04-20 14:14:17 +0900

    a

---
+++ src/4.hook/3.useMemo/1.Factory.js
@@ -0,0 +1,28 @@
+import React, {useState, userMemo} from 'react'
+
+export default function Factory() {
+    const [carCnt, setCarCnt] = useState(0)
+    const [shipCnt, setShipCnt] = useState(0)
+
+    const makeCar = () => setCarCnt(carCnt + 1)
+    const makeShip = () => setShipCnt(shipCnt + 1)
+
+    let porductCnt = carCnt + shipCnt
+
+    return (
+        <>
+            <h1>{porductCnt}</h1>
+            <hr/>
+            <Car make={makeCar}/>{' '}
+            <Ship make={makeShip}/>
+        </>
+    )
+}
+
+function Car({make}) {
+    return <button onClick={make}>car</button>
+}
+
+function Ship({make}) {
+    return <button onClick={make}>ship</button>
+}
